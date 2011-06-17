96c28fbfd97f53ca0950f4e3bb33c84f9de634b1
Andrew Kushnir <kushnir.andrew@gmail.com>
2011-06-16 21:37:36 -0700

    ItemPermalink plugin for Echo.Stream application added

---
+++ ItemPermalink/item-permalink.js
@@ -0,0 +1,43 @@
+/*
+ *  ItemPermalink plugin
+ * 
+ *  This Echo Stream client plugin converts static timestamp field into an item
+ *  permanent URL hyperlink. Permanent URL format can be configured through
+ *  the plugin parameters.
+ *
+ *  Version: 2.0 (06/16/2011)
+ *  Developed by: Echo Solutions team (Kushnir Andrew)
+ *  Documentation: http://wiki.aboutecho.com/Community-Developed-Plugins#ItemPermalink
+ *
+ *  More info about Echo StreamServer: http://www.aboutecho.com/
+ *
+ *  Disclaimer: this software is provided by the author "AS IS" and no warranties are implied
+ */
+
+(function($){
+
+var plugin = Echo.createPlugin({
+	"name": "ItemPermalink",
+	"applications": ["Stream"],
+	"init": function(plugin, application) {
+		plugin.extendRenderer("Item", "date", plugin.renderers.Item.date);
+	}
+});
+
+plugin.renderers = {"Item": {}};
+
+plugin.renderers.Item.date = function(element) {
+	var item = this;
+	item.parentRenderer("date", arguments);
+	var format = plugin.config.get(item, "format");
+	if (!format) return;
+	var href = $.isFunction(format)
+		? format(item)
+		: format.replace(/\[ID\]/g, item.data.object.id);
+	element.wrap(item.hyperlink(
+		{"href": href, "skipEscaping": true},
+		{"openInNewWindow": plugin.config.get(item, "openInNewWindow", "", true)}
+	));
+};
+	
+})(jQuery);
