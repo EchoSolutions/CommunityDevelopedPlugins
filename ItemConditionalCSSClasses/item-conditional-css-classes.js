37e4d209bdf6fd88add6d3df82768f36fb2ebcf0
Andrew Kushnir <kushnir.andrew@gmail.com>
2011-10-28 13:03:44 -0700

    New parameter "caseInsensitive" was added to the plugin configuration to allow case insensitive values comparision

--- ItemConditionalCSSClasses/item-conditional-css-classes.js
+++ ItemConditionalCSSClasses/item-conditional-css-classes.js
@@ -5,7 +5,7 @@
  *  container DOM element based on a certain criteria (like a user id/role/state
  *  or item state). It allows to apply specific look and feel for the item in the stream.
  *
- *  Version: 2.0 (06/16/2011)
+ *  Version: 2.1 (10/28/2011)
  *  Developed by: Echo Solutions team (Kushnir Andrew)
  *  Documentation: http://wiki.aboutecho.com/Community-Developed-Plugins#ItemConditionalCSSClasses
  *
@@ -33,11 +33,26 @@ plugin.renderers.Item.content = function(element) {
 	if (!conditions || !conditions.length) return;
 	$.map(conditions, function(condition) {
 		var value = $.getNestedValue(condition.field, item.data);
-		if (($.isArray(value) && $.inArray(condition.value, value) > -1)
-			|| condition.value == value) {
-				element.addClass(condition.className);
+		var isCaseInsensitive = plugin.config.get(item, "caseInsensitive") == true;
+		if ($.isArray(value)) {
+			$.each(value, function(_id, _value) {
+				if (plugin.areEqual(_value, condition.value, isCaseInsensitive)) {
+					element.addClass(condition.className);
+					return false; // break
+				}
+			});
+		} else if (plugin.areEqual(condition.value, value, isCaseInsensitive)) {
+			element.addClass(condition.className);
 		}
 	});
+};
+
+plugin.areEqual = function(string1, string2, isCaseInsensitive) {
+	if (isCaseInsensitive) {
+		string1 = (string1 || "").toLowerCase();
+		string2 = (string2 || "").toLowerCase();
+	}
+	return string1 == string2;
 };
 
 })(jQuery);
